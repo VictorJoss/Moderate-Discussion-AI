10249503e317295c7687d223968b215787f8431a
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-09-17 23:56:50 -0500

    initial commit

---
+++ src/main/java/com/victordev/moderatediscussionia/ModerateDiscussionIaApplication.java
@@ -0,0 +1,13 @@
+package com.victordev.moderatediscussionia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerateDiscussionIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModerateDiscussionIaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victordev/moderatediscussionia/ModerateDiscussionIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victordev.moderatediscussionia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerateDiscussionIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
