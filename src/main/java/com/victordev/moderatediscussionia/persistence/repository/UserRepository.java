ffabdba42e3df6ae4b1c186557ee4ae78528ff8e
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-09-27 23:30:10 -0500

    add findByUsername method

--- src/main/java/com/victordev/moderatediscussionia/persistence/repository/UserRepository.java
+++ src/main/java/com/victordev/moderatediscussionia/persistence/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.victordev.moderatediscussionia.persistence.repository;
 
 import com.victordev.moderatediscussionia.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
