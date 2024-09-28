9e3e702b51f08f4856d4127590fda1541cfb128e
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-09-27 23:29:36 -0500

    feat: userDatails implementation

--- src/main/java/com/victordev/moderatediscussionia/persistence/entity/User.java
+++ src/main/java/com/victordev/moderatediscussionia/persistence/entity/User.java
@@ -3,13 +3,17 @@ package com.victordev.moderatediscussionia.persistence.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,9 +22,39 @@ public class User {
     private String password;
     private Integer score;
 
+    private boolean accountExpired;
+    private boolean accountLocked;
+    private boolean credentialsExpired;
+    private boolean enabled;
+
     @OneToMany(mappedBy = "user")
     private List<Debate> debates;
 
     @OneToMany(mappedBy = "user")
     private List<Vote> votes;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !accountExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !credentialsExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
